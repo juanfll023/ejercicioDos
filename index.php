b9da32bb0d234579b1c90e9c492fb14cca7ff1ac
juanfll023 <juanfll023@gmail.com>
2018-02-13 17:50:21 +0100

    Agregando Enlaces de Estilos y Scripts

--- index.php
+++ index.php
@@ -3,11 +3,21 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Ejercicio - Dos - Git</title>
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="css/toastr.css">
+	<link rel="stylesheet" href="css/estilo.css">
 
 </head>
 <body>
 	
-
+	
+	<script src="js/jquery.js"></script>
+	<script src="js/bootstrap.js"></script>
+	<script src="js/vue.js"></script>
+	<script src="js/axios.js"></script>
+	<script src="js/toastr.js"></script>
+	<script src="js/app.js"></script>
 
 </body>
 </html>
